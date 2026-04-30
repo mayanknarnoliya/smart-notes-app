891c1cf9a4964721769cd83376afcfbfea376f55
Mayank Kumar Narnoliya <mayanknarnoliya41@gmail.com>
2026-04-30 23:47:04 +0530

    add production backend URL

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import axios from "axios";
 import App from "./App";
 import "./index.css";
+
+axios.defaults.baseURL = "https://smart-notes-app-a58u.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
